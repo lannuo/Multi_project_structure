d372224f4ef519f3bec1adc6618e5fd75b6a4430
lannuo <lannuo5401@gmail.com>
2017-05-16 13:10:20 +0800

    first commit

---
+++ csdn-web/src/test/java/com/csnd/test/TestDemo.java
@@ -0,0 +1,13 @@
+package com.csnd.test;
+
+import org.junit.Test;
+
+/**
+ * Created by JUN on 2017/5/16.
+ */
+public class TestDemo {
+    @Test
+    public void test(){
+        System.out.println("测试junit依赖");
+    }
+}
